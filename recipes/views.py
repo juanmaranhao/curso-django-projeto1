f6054111f19be0093c3515bf165dcd86cb4ea6ce
Juan Maranhão <juanmaranhao@hotmail.com>
2025-04-24 16:44:40 -0300

    Early tests

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-from utils.recipes.factory import make_recipe
 from .models import Recipe
+
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
-    
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
+
 
 def category(request, category_id):
     recipes = get_list_or_404(
@@ -24,12 +25,13 @@ def category(request, category_id):
         'title': f'{recipes[0].category.name}  - Category | '
     })
 
-def recipe(request, id): 
-    recipe = get_object_or_404(Recipe, 
-            id=id,
-            is_published=True,
-    )
-    
+
+def recipe(request, id):
+    recipe = get_object_or_404(Recipe,
+                               id=id,
+                               is_published=True,
+                               )
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,

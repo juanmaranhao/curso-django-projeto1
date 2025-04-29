bd883d4dcb8025b3d5c53e0e57be3a7f3e6a8db2
Juan Maranhão <juanmaranhao@hotmail.com>
2025-04-28 23:16:18 -0300

    Early pagination

--- recipes/models.py
+++ recipes/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
     def __str__(self):
         return self.name
 
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
@@ -20,9 +22,11 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True

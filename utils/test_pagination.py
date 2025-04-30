09cbe76a77ed49408b4e6203871fa3159f22a10f
Juan Maranhão <juanmaranhao@hotmail.com>
2025-04-30 14:46:52 -0300

    Test pagination

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
+from django.urls import reverse
 from utils.pagination import make_pagination_range
+from recipes.tests.test_recipe_base import RecipeTestBase
 
 
-class PaginationTest(TestCase):
+class PaginationTest(RecipeTestBase, TestCase):
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
@@ -74,3 +76,45 @@ class PaginationTest(TestCase):
             current_page=21,
         )['pagination']
         self.assertEqual([17, 18, 19, 20], pagination)
+
+    def test_pagination_check_recipes_and_pages(self):
+        # response
+        recipes_test = []
+
+        for i in range(8):
+            recipes_test.append(
+                self.make_recipe(
+                    slug=f'slug-{i}',
+                    title=f"Receita da mamãe {i}",
+                    author_data={'username': f'usuario{i}'}
+                )
+            )
+
+        response = reverse('recipes:home')
+
+        response1 = self.client.get(f'{response}?page=1')
+        response_context_recipes_page1 = response1.context['recipes']
+
+        response2 = self.client.get(f'{response}?page=2')
+        response_context_recipes_page2 = response2.context['recipes']
+
+        self.assertEqual(len(response_context_recipes_page1), 6)
+        self.assertEqual(len(response_context_recipes_page2), 2)
+
+        self.assertIn(recipes_test[0], response_context_recipes_page2)
+        self.assertIn(recipes_test[1], response_context_recipes_page2)
+        self.assertIn(recipes_test[2], response_context_recipes_page1)
+        self.assertIn(recipes_test[3], response_context_recipes_page1)
+        self.assertIn(recipes_test[4], response_context_recipes_page1)
+        self.assertIn(recipes_test[5], response_context_recipes_page1)
+        self.assertIn(recipes_test[6], response_context_recipes_page1)
+        self.assertIn(recipes_test[7], response_context_recipes_page1)
+
+    def test_pagination_check_execpt_current_page(self):
+        response = reverse('recipes:home')
+        response_page = self.client.get(f'{response}?page=teste')
+
+        self.assertEqual(
+            1,
+            response_page.context['pagination_range']['current_page']
+        )

be4afb38f54f20fab577a18ebf8bdc0301b9d16f
Juan Maranhão <juanmaranhao@hotmail.com>
2025-05-03 01:30:32 -0300

    Login and logout

--- authors/forms/register_form.py
+++ authors/forms/register_form.py
@@ -1,33 +1,11 @@
-import re
 from django import forms
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from utils.django_forms import add_placeholder, strong_password
 # from django.contrib.auth.models import User
 
 
 User = get_user_model()
-
-
-def add_attr(field, attr_name, attr_new_val):
-    existing_attr = field.widget.attrs.get(attr_name, '')
-    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
-
-
-def add_placeholder(field, placeholder_val):
-    add_attr(field, 'placeholder', placeholder_val)
-
-
-def strong_password(password):
-    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
-
-    if not regex.match(password):
-        raise ValidationError((
-            'Password must have at least one uppercase letter, '
-            'one lowercase letter and one number. The length should be '
-            'at least 8 characters.'
-        ),
-            code='Invalid'
-        )
 
 
 class RegisterForm(forms.ModelForm):

12643407b255cfd53556f689b1c279ece37aaf05
Juan Maranhão <juanmaranhao@hotmail.com>
2025-05-09 22:05:45 -0300

    Menu e Profile created

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS: list[str] = ['cursodjango.infinitumsol.com.br']
+ALLOWED_HOSTS: list[str] = [
+    'cursodjango.infinitumsol.com.br',
+    '127.0.0.1'
+]
 
 
 # Application definition

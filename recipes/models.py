c64dc18964f3db3deea8efed52466821b57c6411
Juan Maranhão <juanmaranhao@hotmail.com>
2025-05-11 19:41:35 -0300

    Tratando imagens

--- recipes/models.py
+++ recipes/models.py
@@ -5,7 +5,9 @@ from django.forms import ValidationError
 from django.urls import reverse
 from django.utils.text import slugify
 from tag.models import Tag
-
+import os
+from django.conf import settings
+from PIL import Image
 
 User = get_user_model()
 
@@ -47,12 +49,44 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse('recipes:recipe', args=(self.id,))
 
+    @staticmethod
+    def resize_image(image, new_width=800):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_height = image_pillow.size
+
+        if original_width <= new_width:
+            print('Image is already smaller than the new width.')
+            image_pillow.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_image = image_pillow.resize(
+            (new_width, new_height), Image.LANCZOS
+        )
+
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=50,
+        )
+        print('Image resized successfully.')
+
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
 
-        return super().save(*args, **kwargs)
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 840)
+            except FileNotFoundError:
+                ...
+
+        return saved
 
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list)
